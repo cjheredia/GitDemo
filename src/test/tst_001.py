78a7c5a844b6e2cab8d366b747e03f9a6bcf8a2b
cjheredia <claudiajimenezheredia@gmail.com>
2021-11-15 09:05:37 -0500

    second commit from Assian timezone guy

--- src/test/tst_001.py
+++ src/test/tst_001.py
@@ -10,9 +10,11 @@ class Test_001 (unittest.TestCase):
         Variable_A = 6
         Variable_B = 4
         self.Resultado = Variable_A + Variable_B
+        print(self.Resultado)
 
     def tearDown(self):
         self.assertTrue (self.Resultado == 10, "El valor no es 10, el valor es (self.Resultado)")
+
 
 
 if __name__ == '__main__':
